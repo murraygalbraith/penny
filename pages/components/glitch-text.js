ae4247339009e7ba25b4b8a7b57829e79ac9bd7c
murraygalbraith <experiments@murraygalbraith.com>
2025-03-24 21:38:53 +1000

    Update glitch-text.js

--- pages/components/glitch-text.js
+++ pages/components/glitch-text.js
@@ -1,1 +1,9 @@
+import React from 'react'
 
+export const GlitchText = ({ children }) => {
+  return (
+    <span className="relative inline-block">
+      <span className="relative z-10">{children}</span>
+    </span>
+  )
+}
